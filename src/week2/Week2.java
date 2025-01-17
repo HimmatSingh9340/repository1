1f9ef039421ea87c8dfea32dbb462ba8b096daae
HimmatSingh9340 <sin14257@sheridancollege.ca>
2025-01-17 16:14:56 -0500

    2nd comment

--- src/week2/Week2.java
+++ src/week2/Week2.java
@@ -16,6 +16,7 @@ public class Week2 {
     public static void main(String[] args) {
         System.out.println("Hi");
         //commit again
+        //change
         // TODO code application logic here
     }
     
