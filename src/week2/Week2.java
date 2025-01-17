0f93ce8582db242ff83e6217ef7d7f8e55677d87
HimmatSingh9340 <sin14257@sheridancollege.ca>
2025-01-17 15:49:48 -0500

    Revert "Revert "commit again comment""
    This reverts commit 03fcff01783406b10c9df6b602486c7b86508b70.

--- src/week2/Week2.java
+++ src/week2/Week2.java
@@ -15,6 +15,7 @@ public class Week2 {
      */
     public static void main(String[] args) {
         System.out.println("Hi");
+        //commit again
         // TODO code application logic here
     }
     
